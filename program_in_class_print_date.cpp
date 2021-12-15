3fb27c3283d4ab4277c4de72c99f971cee87b688
Bhuwanesh Tripathi <Bhuwanesh340@gmail.com>
2021-12-15 19:04:46 +0530

    program in class to print date

---
+++ program_in_class_print_date.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class date
+{
+    private:
+     int d;
+     int mm;
+     int yy;
+
+    public:
+      int d=22;
+     int  mm=12;
+      int yy=2018;
+
+    void display(){
+        cout<<d<<"-"<<mm<<"-"<<yy;
+    }
+};
+int main()
+{
+    date obj;
+    obj.display();
+    return 0;
+}
